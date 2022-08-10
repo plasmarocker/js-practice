bf5a0b219d0287466c05ffbbd4621aba37019f35
Jonathan Carter <carterj@belletire.com>
2022-08-10 16:59:01 -0400

    added second callback exercise

---
+++ 02 - medium/callbacks2.js
@@ -0,0 +1,17 @@
+function moo(mooCompleteCallback) {
+    console.log('About to moo')
+    console.log('MOOOOO!')
+
+}
+
+
+// complete the above function so that you see "finished mooing. right!" on the console
+
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+function mooComplete() {
+    console.log('finished mooing. right!')
+}
+
+moo(mooComplete);
